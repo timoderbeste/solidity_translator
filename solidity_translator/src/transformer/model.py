bbaf9ad4762bebb744ec6331087664c7458ea925
Timo Wang <timowang@Timos-iMac.local>
2019-02-28 02:34:48 -0600

    Adding transformer codeto the project

--- solidity_translator/src/transformer/model.py
+++ solidity_translator/src/transformer/model.py
@@ -12,6 +12,11 @@ import math, copy, time
 from torch.autograd import Variable
 
 
+# A list of todos.
+# TODO define the vocab as well as the embedding for the restricted natural language
+# TODO define the vocab as well as the embedding for the solidity code
+
+
 class EncoderDecoder(nn.Module):
     """
     A standard Encoder-Decoder architecture. Base for this and many
@@ -70,16 +75,78 @@ class Encoder(nn.Module):
         self.norm = LayerNorm(layer.size)
     
     def forward(self, x, mask):
-        "Pass the input (and mask) through each layer in turn."
+        """
+        Pass the input (and mask) through each layer in turn.
+        """
         for layer in self.layers:
             x = layer(x, mask)
         return self.norm(x)
-    
+
+
+class EncoderLayer(nn.Module):
+    """
+    Encoder is made up of self-attn and feed forward (defined below)
+    """
+
+    def __init__(self, size, self_attn, feed_forward, dropout):
+        super(EncoderLayer, self).__init__()
+        self.self_attn = self_attn
+        self.feed_forward = feed_forward
+        self.sublayer = clones(SublayerConnection(size, dropout), 2)
+        self.size = size
+
+    def forward(self, x, mask):
+        """
+        Follow Figure 1 (left) for connections.
+        """
+        x = self.sublayer[0](x, lambda _x: self.self_attn(_x, _x, _x, mask))
+        return self.sublayer[1](x, self.feed_forward)
+
+
+class Decoder(nn.Module):
+    """
+    Generic N layer decoder with masking.
+    """
+
+    def __init__(self, layer, N):
+        super(Decoder, self).__init__()
+        self.layers = clones(layer, N)
+        self.norm = LayerNorm(layer.size)
+
+    def forward(self, x, memory, src_mask, tgt_mask):
+        for layer in self.layers:
+            x = layer(x, memory, src_mask, tgt_mask)
+        return self.norm(x)
+
+
+class DecoderLayer(nn.Module):
+    """
+    Decoder is made of self-attn, src-attn, and feed forward (defined below)
+    """
+
+    def __init__(self, size, self_attn, src_attn, feed_forward, dropout):
+        super(DecoderLayer, self).__init__()
+        self.size = size
+        self.self_attn = self_attn
+        self.src_attn = src_attn
+        self.feed_forward = feed_forward
+        self.sublayer = clones(SublayerConnection(size, dropout), 3)
+
+    def forward(self, x, memory, src_mask, tgt_mask):
+        """
+        Follow Figure 1 (right) for connections.
+        """
+        m = memory
+        x = self.sublayer[0](x, lambda _x: self.self_attn(_x, _x, _x, tgt_mask))
+        x = self.sublayer[1](x, lambda _x: self.src_attn(_x, m, m, src_mask))
+        return self.sublayer[2](x, self.feed_forward)
+
 
 class LayerNorm(nn.Module):
     """
     Construct a layernorm module (See citation for details).
     """
+    # features is simply an int indicating the size of input.
     def __init__(self, features, eps=1e-6):
         super(LayerNorm, self).__init__()
         self.a_2 = nn.Parameter(torch.ones(features))
@@ -107,64 +174,6 @@ class SublayerConnection(nn.Module):
         Apply residual connection to any sublayer with the same size.
         """
         return x + self.dropout(sublayer(self.norm(x)))
-    
-    
-class EncoderLayer(nn.Module):
-    """
-    Encoder is made up of self-attn and feed forward (defined below)
-    """
-    def __init__(self, size, self_attn, feed_forward, dropout):
-        super(EncoderLayer, self).__init__()
-        self.self_attn = self_attn
-        self.feed_forward = feed_forward
-        self.sublayer = clones(SublayerConnection(size, dropout), 2)
-        self.size = size
-
-    def forward(self, x, mask):
-        """
-        Follow Figure 1 (left) for connections.
-        """
-        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, mask))
-        return self.sublayer[1](x, self.feed_forward)
-
-
-class Decoder(nn.Module):
-    """
-    Generic N layer decoder with masking.
-    """
-    
-    def __init__(self, layer, N):
-        super(Decoder, self).__init__()
-        self.layers = clones(layer, N)
-        self.norm = LayerNorm(layer.size)
-    
-    def forward(self, x, memory, src_mask, tgt_mask):
-        for layer in self.layers:
-            x = layer(x, memory, src_mask, tgt_mask)
-        return self.norm(x)
-
-
-class DecoderLayer(nn.Module):
-    """
-    Decoder is made of self-attn, src-attn, and feed forward (defined below)
-    """
-    
-    def __init__(self, size, self_attn, src_attn, feed_forward, dropout):
-        super(DecoderLayer, self).__init__()
-        self.size = size
-        self.self_attn = self_attn
-        self.src_attn = src_attn
-        self.feed_forward = feed_forward
-        self.sublayer = clones(SublayerConnection(size, dropout), 3)
-    
-    def forward(self, x, memory, src_mask, tgt_mask):
-        """
-        Follow Figure 1 (right) for connections.
-        """
-        m = memory
-        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, tgt_mask))
-        x = self.sublayer[1](x, lambda x: self.src_attn(x, m, m, src_mask))
-        return self.sublayer[2](x, self.feed_forward)
     
     
 def subsequent_mask(size):
